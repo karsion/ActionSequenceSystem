43b454df197b20b2b894eafeb0cf2ac635f24074
karsion <karsion@126.com>
2018-02-20 12:04:32 +0800

    修复使用相同ID停止ActionSequence后再次开启ActionSequence时不能开启的BUG

--- Assets/Tools/ActionSequenceSystem/ActionSequenceSystem.cs
+++ Assets/Tools/ActionSequenceSystem/ActionSequenceSystem.cs
@@ -24,15 +24,15 @@ public static class ActionSequenceSystemEx
     }
 
     //直接延迟动作
-    public static Component Delayer(this Component id, float delay, Action action)
+    public static ActionSequence Delayer(this Component id, float delay, Action action)
     {
         ActionSequence seq = ActionSequenceSystem.GetSequence(id);
         seq.Interval(delay).Action(action);
-        return id;
+        return seq;
     }
 
     //直接循环动作
-    public static Component Looper(this Component id, float interval, int loopTime, bool isActionAtStart, Action action)
+    public static ActionSequence Looper(this Component id, float interval, int loopTime, bool isActionAtStart, Action action)
     {
         ActionSequence seq = ActionSequenceSystem.GetSequence(id);
         if (isActionAtStart)
@@ -45,7 +45,7 @@ public static class ActionSequenceSystemEx
         }
 
         seq.Loop(loopTime);
-        return id;
+        return seq;
     }
 
     //直接循环动作
@@ -68,9 +68,15 @@ public static class ActionSequenceSystemEx
 
 public class ActionSequenceSystem : SingletonMono<ActionSequenceSystem>
 {
-    private readonly List<Component> listID = new List<Component>(64);
     private readonly List<ActionSequence> listSequence = new List<ActionSequence>(64);
     public List<ActionSequence> ListSequence { get { return listSequence; } }
+
+#if UNITY_EDITOR
+    private void Reset()
+    {
+        name = "ActionSequenceSystem";
+    }
+#endif
 
     //开动作序列
     public static ActionSequence GetSequence(Component component)
@@ -83,23 +89,6 @@ public class ActionSequenceSystem : SingletonMono<ActionSequenceSystem>
     // Update is called once per frame
     private void Update()
     {
-        //StopSequence by ID
-        if (listID.Count > 0)
-        {
-            for (int i = 0; i < listID.Count; i++)
-            {
-                for (int j = 0; j < listSequence.Count; j++)
-                {
-                    if (listID[i] == listSequence[j].id)
-                    {
-                        listSequence[j].Stop();
-                    }
-                }
-            }
-
-            listID.Clear();
-        }
-
         //UpdateSequence
         bool isNeedRemoveSequence = false;
         for (int i = 0; i < listSequence.Count; i++)
@@ -119,8 +108,19 @@ public class ActionSequenceSystem : SingletonMono<ActionSequenceSystem>
         }
     }
 
-    internal static void SetStopSequenceID(Component id)
+    private void StopSequenceByID(Component id)
     {
-        instance.listID.Add(id);
+        for (int i = 0; i < listSequence.Count; i++)
+        {
+            if (id == listSequence[i].id)
+            {
+                listSequence[i].Stop();
+            }
+        }
+    }
+
+    public static void SetStopSequenceID(Component id)
+    {
+        instance.StopSequenceByID(id);
     }
 }
