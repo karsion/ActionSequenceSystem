91bb12511c5c70d11e63cd5e3e2cdf5a46b33972
Karsion <karsion@126.com>
2021-08-11 23:02:58 +0800

    Update ActionSequenceSystemEx.cs
    自动格式

--- ActionSequenceSystem/ComponentExtension/ActionSequenceSystemEx.cs
+++ ActionSequenceSystem/ComponentExtension/ActionSequenceSystemEx.cs
@@ -32,47 +32,38 @@ namespace UnrealM
         }
 
         #region Start Stop
+
         /// <summary>
         /// 用Component作为ID开序列
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        public static ActionSequence Sequence(this Component id)
-        {
-            return ActionSequenceSystem.Sequence(id);
-        }
+        public static ActionSequence Sequence(this Component id) => ActionSequenceSystem.Sequence(id);
 
         /// <summary>
         /// 用Component作为ID停止序列
         /// </summary>
         /// <param name="id"></param>
-        public static void StopSequence(this Component id)
-        {
-            ActionSequenceSystem.StopSequence(id);
-        }
+        public static void StopSequence(this Component id) { ActionSequenceSystem.StopSequence(id); }
 
         /// <summary>
         /// 用Component作为ID停止指定的序列
         /// </summary>
         /// <param name="id"></param>
         /// <param name="sequence">停止指定的ActionSequence</param>
-        public static void StopSequence(this Component id, ActionSequence sequence)
-        {
-            sequence?.Stop(id);
-        }
+        public static void StopSequence(this Component id, ActionSequence sequence) { sequence?.Stop(id); }
+
         #endregion
 
         #region Enabler Disabler
+
         /// <summary>
         /// 延迟启用ID的Behaviour
         /// </summary>
         /// <param name="id"></param>
         /// <param name="delay">延迟</param>
         /// <returns></returns>
-        public static ActionSequence Enabler(this Behaviour id, float delay)
-        {
-            return Sequence(id).Interval(delay).Enable();
-        }
+        public static ActionSequence Enabler(this Behaviour id, float delay) => Sequence(id).Interval(delay).Enable();
 
         /// <summary>
         /// 延迟禁用ID的Behaviour
@@ -80,10 +71,8 @@ namespace UnrealM
         /// <param name="id"></param>
         /// <param name="delay">延迟</param>
         /// <returns></returns>
-        public static ActionSequence Disabler(this Behaviour id, float delay)
-        {
-            return Sequence(id).Interval(delay).Disable();
-        }
+        public static ActionSequence Disabler(this Behaviour id, float delay) => Sequence(id).Interval(delay).Disable();
+
         #endregion
 
         #region Shower Hider
@@ -92,10 +81,7 @@ namespace UnrealM
         /// 停止延迟激活的GameObject(ID是Transform)
         /// </summary>
         /// <param name="go"></param>
-        public static void StopShowerHider(this GameObject go)
-        {
-            StopSequence(go.transform);
-        }
+        public static void StopShowerHider(this GameObject go) { StopSequence(go.transform); }
 
         /// <summary>
         /// 延迟激活的GameObject(ID是Transform)
@@ -103,10 +89,8 @@ namespace UnrealM
         /// <param name="go"></param>
         /// <param name="delay">延迟</param>
         /// <returns></returns>
-        public static ActionSequence Shower(this GameObject go, float delay)
-        {
-            return Sequence(go.transform).Interval(delay).Show();
-        }
+        public static ActionSequence Shower(this GameObject go, float delay) =>
+            Sequence(go.transform).Interval(delay).Show();
 
         /// <summary>
         /// 延迟激活ID的GameObject
@@ -114,10 +98,7 @@ namespace UnrealM
         /// <param name="id"></param>
         /// <param name="delay">延迟</param>
         /// <returns></returns>
-        public static ActionSequence Shower(this Component id, float delay)
-        {
-            return Sequence(id).Interval(delay).Show();
-        }
+        public static ActionSequence Shower(this Component id, float delay) => Sequence(id).Interval(delay).Show();
 
         /// <summary>
         /// 延迟反激活ID的GameObject(ID是Transform)
@@ -125,10 +106,8 @@ namespace UnrealM
         /// <param name="go"></param>
         /// <param name="delay">延迟</param>
         /// <returns></returns>
-        public static ActionSequence Hider(this GameObject go, float delay)
-        {
-            return Sequence(go.transform).Interval(delay).Hide();
-        }
+        public static ActionSequence Hider(this GameObject go, float delay) =>
+            Sequence(go.transform).Interval(delay).Hide();
 
         /// <summary>
         /// 延迟反激活ID的GameObject
@@ -136,10 +115,8 @@ namespace UnrealM
         /// <param name="id"></param>
         /// <param name="delay">延迟</param>
         /// <returns></returns>
-        public static ActionSequence Hider(this Component id, float delay)
-        {
-            return Sequence(id).Interval(delay).Hide();
-        }
+        public static ActionSequence Hider(this Component id, float delay) => Sequence(id).Interval(delay).Hide();
+
         #endregion
 
         #region Delayer Looper NextFramer WaitFor
@@ -152,10 +129,8 @@ namespace UnrealM
         /// <param name="action">调用的函数</param>
         /// <param name="actionId">用于区分多路回调</param>
         /// <returns></returns>
-        public static ActionSequence Delayer(this Component id, float delay, IAction action, int actionId)
-        {
-            return Sequence(id).Interval(delay).IAction(action, actionId);
-        }
+        public static ActionSequence Delayer(this Component id, float delay, IAction action, int actionId) =>
+            Sequence(id).Interval(delay).IAction(action, actionId);
 
         /// <summary>
         /// 延迟调用函数
@@ -164,10 +139,8 @@ namespace UnrealM
         /// <param name="delay">延迟</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Delayer(this Component id, float delay, Action action)
-        {
-            return Sequence(id).Interval(delay).Action(action);
-        }
+        public static ActionSequence Delayer(this Component id, float delay, Action action) =>
+            Sequence(id).Interval(delay).Action(action);
 
         /// <summary>
         /// 延迟调用IAction接口，循环次数作为参数
@@ -177,10 +150,8 @@ namespace UnrealM
         /// <param name="action">调用的函数</param>
         /// <param name="actionId">用于区分多路回调</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float delay, IAction action, int actionId)
-        {
-            return Sequence(id).Interval(delay).IAction(action, actionId).Loop();
-        }
+        public static ActionSequence Looper(this Component id, float delay, IAction action, int actionId) =>
+            Sequence(id).Interval(delay).IAction(action, actionId).Loop();
 
         /// <summary>
         /// 无限循环调用函数，循环次数作为参数
@@ -189,10 +160,8 @@ namespace UnrealM
         /// <param name="interval">延迟</param>
         /// <param name="action">调用的函数，循环次数作为参数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float interval, Action<int> action)
-        {
-            return Sequence(id).Interval(interval).Action(action).Loop();
-        }
+        public static ActionSequence Looper(this Component id, float interval, Action<int> action) =>
+            Sequence(id).Interval(interval).Action(action).Loop();
 
         /// <summary>
         /// 无限循环调用函数
@@ -201,10 +170,8 @@ namespace UnrealM
         /// <param name="interval">延迟</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float interval, Action action)
-        {
-            return Sequence(id).Interval(interval).Action(action).Loop();
-        }
+        public static ActionSequence Looper(this Component id, float interval, Action action) =>
+            Sequence(id).Interval(interval).Action(action).Loop();
 
         /// <summary>
         /// 循环调用函数，循环次数作为参数，并设置次数，是否立即开始
@@ -214,10 +181,8 @@ namespace UnrealM
         /// <param name="loopTime">循环调用次数</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float interval, int loopTime, Action action)
-        {
-            return Sequence(id).Interval(interval).Action(action).Loop(loopTime);
-        }
+        public static ActionSequence Looper(this Component id, float interval, int loopTime, Action action) =>
+            Sequence(id).Interval(interval).Action(action).Loop(loopTime);
 
         /// <summary>
         /// 循环调用函数，循环次数作为参数，并设置次数，是否立即开始
@@ -227,10 +192,8 @@ namespace UnrealM
         /// <param name="loopTime">循环调用次数</param>
         /// <param name="action">调用的函数，循环次数作为参数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float interval, int loopTime, Action<int> action)
-        {
-            return Sequence(id).Interval(interval).Action(action).Loop(loopTime);
-        }
+        public static ActionSequence Looper(this Component id, float interval, int loopTime, Action<int> action) =>
+            Sequence(id).Interval(interval).Action(action).Loop(loopTime);
 
         /// <summary>
         /// 循环调用函数，并设置次数，是否立即开始
@@ -241,12 +204,11 @@ namespace UnrealM
         /// <param name="isActionAtStart">是否立即开始</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float interval, int loopTime, bool isActionAtStart, Action action)
-        {
-            return isActionAtStart ?
-                id.Sequence().Action(action).Interval(interval).Loop(loopTime) :
-                id.Looper(interval, loopTime, action);
-        }
+        public static ActionSequence Looper(this Component id, float interval, int loopTime, bool isActionAtStart,
+            Action action) =>
+            isActionAtStart
+                ? id.Sequence().Action(action).Interval(interval).Loop(loopTime)
+                : id.Looper(interval, loopTime, action);
 
         /// <summary>
         /// 循环调用函数，循环次数作为参数，并设置次数，是否立即开始
@@ -257,12 +219,11 @@ namespace UnrealM
         /// <param name="isActionAtStart">是否立即开始</param>
         /// <param name="action">调用的函数，循环次数作为参数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, float interval, int loopTime, bool isActionAtStart, Action<int> action)
-        {
-            return isActionAtStart ?
-                id.Sequence().Action(action).Interval(interval).Loop(loopTime) :
-                id.Looper(interval, loopTime, action);
-        }
+        public static ActionSequence Looper(this Component id, float interval, int loopTime, bool isActionAtStart,
+            Action<int> action) =>
+            isActionAtStart
+                ? id.Sequence().Action(action).Interval(interval).Loop(loopTime)
+                : id.Looper(interval, loopTime, action);
 
         /// <summary>
         /// 延迟一帧调用函数
@@ -270,10 +231,7 @@ namespace UnrealM
         /// <param name="id"></param>
         /// <param name="action"></param>
         /// <returns></returns>
-        public static ActionSequence NextFramer(this Component id, Action action)
-        {
-            return Sequence(id).Action(action);
-        }
+        public static ActionSequence NextFramer(this Component id, Action action) => Sequence(id).Action(action);
 
         /// <summary>
         /// 延迟一帧调用IAction
@@ -282,10 +240,8 @@ namespace UnrealM
         /// <param name="action"></param>
         /// <param name="actionId"></param>
         /// <returns></returns>
-        public static ActionSequence NextFramer(this Component id, IAction action, int actionId)
-        {
-            return Sequence(id).IAction(action, actionId);
-        }
+        public static ActionSequence NextFramer(this Component id, IAction action, int actionId) =>
+            Sequence(id).IAction(action, actionId);
 
         /// <summary>
         /// 等待条件判断成功后调用函数
@@ -294,23 +250,21 @@ namespace UnrealM
         /// <param name="condition">判断函数，函数返回true，调用action</param>
         /// <param name="action">调用函数</param>
         /// <returns></returns>
-        public static ActionSequence WaitFor(this Component id, Func<bool> condition, Action action)
-        {
-            return id.Sequence().WaitFor(condition).Action(action);
-        }
+        public static ActionSequence WaitFor(this Component id, Func<bool> condition, Action action) =>
+            id.Sequence().WaitFor(condition).Action(action);
+
         #endregion
 
         #region Start Delayer Looper with handle
+
         /// <summary>
         /// 用Component作为ID开序列，并使用“控制句柄”
         /// </summary>
         /// <param name="id"></param>
         /// <param name="handle">控制句柄</param>
         /// <returns></returns>
-        public static ActionSequence Sequence(this Component id, ActionSequenceHandle handle)
-        {
-            return ActionSequenceSystem.Sequence(id).SetHandle(handle);
-        }
+        public static ActionSequence Sequence(this Component id, ActionSequenceHandle handle) =>
+            ActionSequenceSystem.Sequence(id).SetHandle(handle);
 
         /// <summary>
         /// 延迟调用IAction接口
@@ -321,10 +275,8 @@ namespace UnrealM
         /// <param name="action">调用的函数</param>
         /// <param name="actionId">用于区分多路回调</param>
         /// <returns></returns>
-        public static ActionSequence Delayer(this Component id, ActionSequenceHandle handle, float delay, IAction action, int actionId)
-        {
-            return Delayer(id, delay, action, actionId).SetHandle(handle);
-        }
+        public static ActionSequence Delayer(this Component id, ActionSequenceHandle handle, float delay,
+            IAction action, int actionId) => Delayer(id, delay, action, actionId).SetHandle(handle);
 
         /// <summary>
         /// 延迟调用函数
@@ -334,10 +286,9 @@ namespace UnrealM
         /// <param name="delay">延迟</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Delayer(this Component id, ActionSequenceHandle handle, float delay, Action action)
-        {
-            return Delayer(id, delay, action).SetHandle(handle);
-        }
+        public static ActionSequence
+            Delayer(this Component id, ActionSequenceHandle handle, float delay, Action action) =>
+            Delayer(id, delay, action).SetHandle(handle);
 
         /// <summary>
         /// 延迟调用IAction接口，循环次数作为参数
@@ -348,10 +299,8 @@ namespace UnrealM
         /// <param name="action">调用的函数</param>
         /// <param name="actionId">用于区分多路回调</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval, IAction action, int actionId)
-        {
-            return Looper(id, interval, action, actionId).SetHandle(handle);
-        }
+        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval,
+            IAction action, int actionId) => Looper(id, interval, action, actionId).SetHandle(handle);
 
         /// <summary>
         /// 无限循环调用函数
@@ -361,10 +310,8 @@ namespace UnrealM
         /// <param name="interval">延迟</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval, Action action)
-        {
-            return Looper(id, interval, action).SetHandle(handle);
-        }
+        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval,
+            Action action) => Looper(id, interval, action).SetHandle(handle);
 
         /// <summary>
         /// 无限循环调用函数
@@ -374,10 +321,8 @@ namespace UnrealM
         /// <param name="interval">延迟</param>
         /// <param name="action">调用的函数，循环次数作为参数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval, Action<int> action)
-        {
-            return Looper(id, interval, action).SetHandle(handle);
-        }
+        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval,
+            Action<int> action) => Looper(id, interval, action).SetHandle(handle);
 
         /// <summary>
         /// 循环调用函数，并设置次数，是否立即开始
@@ -389,10 +334,9 @@ namespace UnrealM
         /// <param name="isActionAtStart">是否立即开始</param>
         /// <param name="action">调用的函数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval, int loopTime, bool isActionAtStart, Action action)
-        {
-            return Looper(id, interval, loopTime, isActionAtStart, action).SetHandle(handle);
-        }
+        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval,
+            int loopTime, bool isActionAtStart, Action action) =>
+            Looper(id, interval, loopTime, isActionAtStart, action).SetHandle(handle);
 
         /// <summary>
         /// 循环调用函数，循环次数作为参数，并设置次数，是否立即开始
@@ -404,10 +348,10 @@ namespace UnrealM
         /// <param name="isActionAtStart">是否立即开始</param>
         /// <param name="action">调用的函数，循环次数作为参数</param>
         /// <returns></returns>
-        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval, int loopTime, bool isActionAtStart, Action<int> action)
-        {
-            return Looper(id, interval, loopTime, isActionAtStart, action).SetHandle(handle);
-        }
+        public static ActionSequence Looper(this Component id, ActionSequenceHandle handle, float interval,
+            int loopTime, bool isActionAtStart, Action<int> action) =>
+            Looper(id, interval, loopTime, isActionAtStart, action).SetHandle(handle);
+
         #endregion
     }
 }
